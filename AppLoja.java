86c8ac66d088cf8b50b9e56970a60befaf1937be
Vitória Ramos <vitoria.ramos@edu.unifil.br>
2025-11-04 20:08:35 -0300

    Update AppLoja.java

--- AppLoja.java
+++ AppLoja.java
@@ -14,24 +14,52 @@ public class AppLoja {
             System.out.println("\nProduto #" + i + ":");
             System.out.print("Código: ");
             int codigo = sc.nextInt();
-            //validar codigo
+
+            // validar codigo
             boolean codigoExiste = p.stream()
                     .anyMatch(prod -> prod.getCodigo() == codigo);
+
             if (codigoExiste) {
-                System.out.println("Código já Cadastrado!");
+                System.out.println(" Código já cadastrado!");
                 i--;
                 continue;
             }
+
             sc.nextLine();
 
             System.out.print("Nome: ");
             String nome = sc.nextLine();
+            
             System.out.print("Preço: ");
             double preco = sc.nextDouble();
+
             p.add(new Produto(codigo, nome, preco));
         }
-       for (Produto pro: p){
-           System.out.println(pro);
-       }
+
+        //aumento
+        System.out.print("\nDigite o código do produto a reajustar: ");
+        int codigoBusca = sc.nextInt();
+
+        Optional<Produto> produtoSelecionado = p.stream()
+                .filter(prod -> prod.getCodigo() == codigoBusca)
+                .findFirst();
+
+        if (produtoSelecionado.isPresent()) {
+            System.out.print("Digite a porcentagem de aumento (%): ");
+            double porcentagem = sc.nextDouble();
+
+            produtoSelecionado.get().aumentoPreco(porcentagem);
+            System.out.println("\n Preço atualizado com sucesso!");
+        } else {
+            System.out.println("\n Codigo não encontrado!");
+        }
+
+        // listagem final
+        System.out.println("\n Lista Atualizada de Produtos:");
+        for (Produto pro : p) {
+            System.out.println(pro);
+        }
+
+        sc.close();
     }
 }
